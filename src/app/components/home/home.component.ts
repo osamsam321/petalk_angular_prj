77f48557a3a65d8c0a2b8cf4045a6542c8b27c9b
osamsam <osamsam321@gmail.com>
2023-03-12 00:52:56 -0600

    changes

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -11,47 +11,60 @@ import { homeservice } from './homeservice';
 export class HomeComponent implements OnInit {
   public user_data: User | undefined;
 
-  constructor(private home_service: homeservice) { }
+  petalk_user!: User;
 
-  ngOnInit(): void {
-   
+  // carousel!: ElementRef;
+
+  // @ViewChild('pRef', {static: false}) pRef: ElementRef;
+
+
+
+  constructor(private homeService:homeservice)
+ { 
+
   }
-  public getPets()
-  {
-    let should_petalk_event_display = true;
-    //tmp
-    let url = "http://localhost/petalk/pet/name/{id}";
-    this.home_service.getPetsWithId(1)
-    
-    .pipe(
-      catchError((error) => this.handleError(error)) 
-    )
-    
-    .subscribe(
-        (userData: User) => {
-           this.user_data= userData;
-
-          console.log("home response details " + userData.first_name+ " " + userData.petalkDevices.values + userData.petsOwnedByUsers.values);
-        },
-        
+  ngOnInit(): void {
+    this.getUser();
+  }
   
+  ngOnChanges(): void{
+  }
+ 
+  ngAfterViewInit()
+  {
+      
+  }
+  handleError(error: any) {
+    // const myParagraphRef = this.elementRef.nativeElement.querySelector('#myParagraph');
+    // if (myParagraphRef) {
+    //   this.renderer2.setProperty(myParagraphRef, 'innerHTML', 'Error occurred');
+    // }
+    console.log('inside of hangle error');
+
+        return throwError(error);
+  }
+  
+  public getUser()
+  {
+    let url = "http://localhost/petalk/pet/name/{id}";
+    
+    this.homeService.getPetsWithId(1)
+ 
+        .pipe(
+          catchError((error) => this.handleError(error)) 
+        )
         
-      )
-           
+        .subscribe(
+            (userData: User) => {
+              this.petalk_user= userData;
+            
+              console.log("response details in home " + userData.first_name+ " " + userData.petalkDevices.values + userData.petsOwnedByUsers.values);
+            },
+          )    
          
     }
+  
+}
 
-    handleError(error: any) {
-      // const myParagraphRef = this.elementRef.nativeElement.querySelector('#myParagraph');
-      // if (myParagraphRef) {
-      //   this.renderer2.setProperty(myParagraphRef, 'innerHTML', 'Error occurred');
-      // }
-      console.log('inside of hangle error');
-  
-          return throwError(error);
-    }
-   
-  
-  }
 
 
--- src/app/components/pet-event-box-simple/pet-event-box-simple.component.ts
+++ src/app/components/pet-event-box-simple/pet-event-box-simple.component.ts
@@ -1,5 +1,5 @@
-import { AfterViewInit, Component, ElementRef, Inject, Injectable, OnInit, Renderer2, ViewChild } from '@angular/core';
-import {PetalkDeviceTrigger, User} from './PetEventBoxSimpleInterface'
+import { AfterViewInit, Component, ElementRef, Inject, Injectable, Input, OnInit, Renderer2, ViewChild } from '@angular/core';
+import {Pet, PetalkDevice, PetalkDeviceTrigger, User} from './PetEventBoxSimpleInterface'
 import { HttpClient, HttpErrorResponse, HttpHeaders } from "@angular/common/http";
 import {PetEventBoxSimpleService} from "./PetEventBoxSimpleService"
 import { NgbCarouselModule } from '@ng-bootstrap/ng-bootstrap/carousel/carousel.module';
@@ -14,14 +14,14 @@ import { catchError, throwError } from 'rxjs';
   templateUrl: './pet-event-box-simple.component.html',
   styleUrls: ['./pet-event-box-simple.component.css', './swiper-bundle.min.css']
 })
-export class PetEventBoxSimpleComponent implements AfterViewInit {
+export class PetEventBoxSimpleComponent implements OnInit, AfterViewInit{
   images = [944, 1011, 984].map((n) => `https://picsum.photos/id/${n}/900/500`);
   http: any;
+  @Input() petalk_user!: User;
   pet_names = [''];
    public petEventAuditList: PetEventAudit[] = [];
   public user_data: User | undefined;
   public petalk_triggers: PetalkDeviceTrigger[] | undefined;
-  test = 'hello'
   @ViewChild('animalName', {static : true}) pet_name_el: ElementRef<HTMLHeadingElement>;
   renderer2: Renderer2;
   @ViewChild('petalk_board_nothing') petalk_board_nothing_el!: ElementRef;
@@ -41,11 +41,30 @@ export class PetEventBoxSimpleComponent implements AfterViewInit {
   }
   
   ngOnChanges(): void{
+    this.updatePetEventAuditList();
   }
- 
+  ngOnInit(): void {
+      this.petalk_user = {
+        id: 0,
+        first_name: '',
+        last_name: '',
+        email: '',
+        username: '',
+        petalkDevices: new Set<PetalkDevice>(),
+        petsOwnedByUsers: new Set<Pet>()
+      };
+  }
   ngAfterViewInit()
   {
-      this.getPet();
+     this.updatePetalkEventDisplayController(); 
+  }
+  updatePetalkEventDisplayController()
+  {
+      if(this.petEventAuditList.length <= 0)
+          this.display_petalk_event_empty_board;
+      else
+          this.display_petalk_event_board();
+
   }
   handleError(error: any) {
     // const myParagraphRef = this.elementRef.nativeElement.querySelector('#myParagraph');
@@ -57,46 +76,24 @@ export class PetEventBoxSimpleComponent implements AfterViewInit {
         return throwError(error);
   }
   
-  public getPet()
+  public updatePetEventAuditList()
   {
-    this.display_petalk_event_empty_board();
-    let should_petalk_event_display = true;
-    let url = "http://localhost/petalk/pet/name/{id}";
-    
-    this.PetEventBoxSimpleService.getPetWithId(1)
+      let petalk_devices  = Array.from(this.petalk_user.petalkDevices);
 
-    
-    .pipe(
-      catchError((error) => this.handleError(error)) 
-    )
-    
-    .subscribe(
-        (userData: User) => {
-           this.user_data= userData;
-           
-           console.log("userdata info {}", userData.email);
-           let petalk_devices = Array.from(this.user_data.petalkDevices);
-           console.log("test" + Array.from(petalk_devices[0].device_name));
-
-              for(let i = 0; i < petalk_devices.length; i++)
-              {
-                let tmp_triggers = Array.from(petalk_devices[i].petalkDeviceTriggers);
-                for(let j = 0; j < tmp_triggers.length;j++)
-                {
-                  console.log(tmp_triggers[j].pet_name + " " +  tmp_triggers[j].triggerType);
-                  this.petEventAuditList.push(new PetEventAudit(tmp_triggers[j].pet_name,tmp_triggers[j].triggerType));
-                }
-              }
-              this.display_petalk_event_board();
-          console.log("response details " + userData.first_name+ " " + userData.petalkDevices.values + userData.petsOwnedByUsers.values);
-        },
-
-      )
-      console.log("done");
-  
-     
-         
+      for(let i = 0; i < petalk_devices.length; i++)
+      {
+        let tmp_triggers = Array.from(petalk_devices[i].petalkDeviceTriggers);
+        for(let j = 0; j < tmp_triggers.length;j++)
+        {
+          console.log(tmp_triggers[j].pet_name + " " +  tmp_triggers[j].triggerType);
+          this.petEventAuditList.push(new PetEventAudit(tmp_triggers[j].pet_name,tmp_triggers[j].triggerType));
+        }
+      }
+      this.display_petalk_event_board();
+    // console.log("response details " + userData.first_name+ " " + userData.petalkDevices.values + userData.petsOwnedByUsers.values);
+    console.log("userdate from the home obj " + this.petalk_user.first_name+ " " + this.petalk_user.petalkDevices.values + this.petalk_user.petsOwnedByUsers.values);
     }
+    
     
     public display_petalk_event_empty_board()
     {
