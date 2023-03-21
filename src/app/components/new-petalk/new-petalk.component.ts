dce6e82da7461e5a2c7522f59eb07cd86b50e45e
osamsam <osamsam321@gmail.com>
2023-03-20 22:47:23 -0500

    ye[

--- src/app/components/new-petalk/new-petalk.component.ts
+++ src/app/components/new-petalk/new-petalk.component.ts
@@ -1,4 +1,7 @@
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Component, ElementRef, OnInit, Renderer2, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Pet } from '../pet-event-box-simple/PetEventBoxSimpleInterface';
 
 @Component({
   selector: 'app-new-petalk',
@@ -7,7 +10,7 @@ import { Component, ElementRef, OnInit, Renderer2, ViewChild } from '@angular/co
 })
 export class NewPetalkComponent implements OnInit {
 @ViewChild('tstButton', {static: true}) tst_btn_el: ElementRef<HTMLInputElement>;
-  constructor(Render: Renderer2, tst_btn_el: ElementRef<HTMLInputElement>) { 
+  constructor(Render: Renderer2, tst_btn_el: ElementRef<HTMLInputElement>, private http: HttpClient) { 
 
     this.tst_btn_el = tst_btn_el;
   }
@@ -20,5 +23,26 @@ export class NewPetalkComponent implements OnInit {
     // this.http
   }
 
+  submitNewPetalkPet(newPetForm: NgForm)
+  {
 
-}
+      let temp_url = "http://localhost:8002/petalk/new/petalk_pet";
+      this.http.post<Pet>(temp_url, newPetForm.value).subscribe(
+  
+        () => {
+          console.log("data sent over");
+          newPetForm.reset();
+          alert("your message was succesfully sent over");
+  
+  
+          (error: HttpErrorResponse) => {
+            alert("there was an error with the request");
+          }
+        }
+      );
+    }
+  
+  }
+  
+
+
